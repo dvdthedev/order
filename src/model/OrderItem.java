2cb0c89fdf1abd45e4333b63d2bc9740b05e4910
Deivid Rocha <deividzor@gmail.com>
2025-01-03 17:50:03 -0300

    first_commit

---
+++ src/model/OrderItem.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderItem {
+    private Integer quantity;
+    private Double price;
+
+    private List<Product> products = new ArrayList<>();
+
+    public void subTotal(){
+        
+    }
+}
